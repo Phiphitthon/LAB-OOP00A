c0dc5e4da2084a5ea073beec774f03d427d9d547
Phiphitthon <phiphitthon.p@kkumail.com>
2022-11-28 15:52:22 +0700

    summary

---
+++ LAB-OOP00A/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Phiphitthon Phiphithatkul");
+Console.WriteLine("653450097-2");
+Console.WriteLine("phiphitthon.p@kkumail.com");
