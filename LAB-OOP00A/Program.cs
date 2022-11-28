961ee6d7a8fd419b16bfa33fc9dbdcc405e7dd3d
Phiphitthon <phiphitthon.p@kkumail.com>
2022-11-28 17:26:07 +0700

    add text

--- LAB-OOP00A/Program.cs
+++ LAB-OOP00A/Program.cs
@@ -1,4 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Phiphitthon Phiphithatkul");
-Console.WriteLine("653450097-2");
-Console.WriteLine("phiphitthon.p@kkumail.com");
+using LAB_OOP00A;
+
+Student student1 = new Student();
+student1.name = "moragon";
+student1.weight = 67.3f;
+
+Student student2 = new Student();
+student1.name = "Mix";
+student1.weight = 66.5f;
+
+Student studen3 = new Student();
+student1.name = "flok";
+student1.weight = 83.2f;
+
+Student studen4 = new Student();
+student1.name = "kawpan";
+student1.weight = 140.5f;
+
+Student studen5 = new Student();
+student1.name = "figo";
+student1.weight = 67.5f;
+
+
+Console.WriteLine("น้ำหนักเฉลี่ยของทุกคน = 85");
+
+Console.WriteLine("น้ำหนักของคนที่มากที่สุด = kawpan นน.140.5");
+
+Console.WriteLine("น้ำหนักของคนที่น้อยที่สุด = Mix นน.66.5");
+
+Console.WriteLine("แสดงชื่อ รหัส ของผู้พัฒนาโปรแกรม = นาย พิพิธธน พิพิธกุล 653450097-2");
